9a832941cefb54885ca1a859bb803e291332ca3f
Alex <alexiv.04@mail.ru>
2022-10-17 12:16:26 +0700

    opisanie1

--- app/src/main/java/com/example/ourseproject/MainActivity.java
+++ app/src/main/java/com/example/ourseproject/MainActivity.java
@@ -18,15 +18,34 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    Объявление констант REQUEST_CODE с произвольными значениями
+    Эти константы используются в интентах и запросах разрешений, чтобы отличать друг от друга пришедшие результаты(30)
+     */
     private static final int RECORD_REQUEST_CODE  = 101;
     private static final int STORAGE_REQUEST_CODE = 102;
     private static final int AUDIO_REQUEST_CODE   = 103;
 
+    /*
+    Обявление перемнных класса
+    MediaProjectionManager - управляет получением  определенных типов токеном MediaProjection
+    MediaProjection - токен, предоставляющий приложению возможность захватить содержимое экрана или записывать аудиосистемы
+    Обявление экземпляра нашего сервиса RecordService и обычной кнопки
+     */
     private MediaProjectionManager projectionManager;
     private MediaProjection mediaProjection;
     private RecordService recordService;
     private Button startBtn;
 
+    /*
+    onCreate - получаем экземпляр MediaProjectionManager для управления сессиями отображения данных
+    Создаем кнопку(поумаолчанию неактивную) и слушатель для нее
+       В методе onClick по нажатию на кнопку, вызывается метод recordService.stopRecord (в случае, если запись идет)
+       иначе создаем интент с projectionManager.createScreenCaptureIntent и отправляем его методом startActivityForResult
+       Далее идут запросы разрешений на запись в память устройства и на запись аудио.
+       Здесь создаем еще один интент для запуска сервиса и передаем его методу bindService, который выполняет привязку сервиса к приложению, а если сервис не работает, то стартует его предварительно
+
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,12 +83,20 @@ public class MainActivity extends AppCompatActivity {
         bindService(intent, connection, BIND_AUTO_CREATE);
     }
 
+    /*
+    В методе onDestroy отвязываем сервис
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unbindService(connection);
     }
 
+    /*
+    Методе onActivityResult получает результат вызова метода StartActivityForResult, где отправляется капчер интент и RECORD_REQUEST_CODE
+    Если запрос прошел успешно, то создадим объект mediaProjection, полученный от успешного запроса захвата экрана. Он будет иметь значение null, если результат от StartActivityForResult будет не окей.
+    Вызываем метод recordService.startRecord и меняем текст кнопки на остановить запиись
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
